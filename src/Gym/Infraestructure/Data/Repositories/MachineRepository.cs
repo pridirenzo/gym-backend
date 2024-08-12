84f38324836cba8550e0941d0648b65554171d57
BERE <bereailsolohaga@hotmail.com>
2024-08-12 11:32:22 -0300

    machine endpoints

---
+++ src/Gym/Infraestructure/Data/Repositories/MachineRepository.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Entities;
+using Infraestructure.Data;
+using Infrastructure.Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Infrastructure.Data.Repositories
+{
+    public class MachineRepository : IMachineRepository
+    {
+        private readonly GymContext _context;
+
+        public MachineRepository(GymContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Machine>> GetAllMachines()
+        {
+            return await _context.Machines.ToListAsync();
+        }
+
+        public async Task<Machine> GetMachineById(int id)
+        {
+            return await _context.Machines.FirstOrDefaultAsync(m => m.MachineId == id);
+        }
+    }
+}
---
+++ src/Gym/Web/Controllers/MachineController.cs
@@ -0,0 +1,44 @@
+﻿using Infrastructure.Data.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Web.Controllers
+{
+    [Route("api/machine")]
+    [ApiController]
+    public class MachineController : ControllerBase
+    {
+        private readonly IMachineRepository _machineRepository;
+
+        public MachineController(IMachineRepository machineRepository)
+        {
+            _machineRepository = machineRepository;
+        }
+
+        [HttpGet("machines")]
+        public async Task<IActionResult> GetAllMachines()
+        {
+            var machines = await _machineRepository.GetAllMachines();
+
+            if (machines == null || !machines.Any())
+            {
+                return NotFound("No machines found");
+            }
+
+            return Ok(machines);
+        }
+
+        [HttpGet("{machineId}")]
+        public async Task<IActionResult> GetMachineById(int machineId)
+        {
+            var machine = await _machineRepository.GetMachineById(machineId);
+
+            if (machine == null)
+            {
+                return NotFound("Machine not found");
+            }
+
+            return Ok(machine);
+        }
+    }
+}
