101429e6a1a55fc48337e0c250c753df3df4aee8
Santiago Feresin <santiagoferesin@gmail.com>
2024-08-29 01:50:31 -0300

    Automapper profile

--- src/Gym/Application/Profiles/ExerciseProfile.cs
+++ src/Gym/Application/Profiles/ExerciseProfile.cs
@@ -8,8 +8,17 @@ namespace Application.Profiles
     {
         public ExerciseProfile()
         {
-            CreateMap<Exercise, ExerciseDto>();
-            CreateMap<ExerciseDto, Exercise>();
+            CreateMap<Exercise, ExerciseDto>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.ExerciseId))
+                .ForMember(dest => dest.Category, opt => opt.MapFrom(src => src.Category))
+                .ForMember(dest => dest.MachineId, opt => opt.MapFrom(src => src.MachineId));
+
+            CreateMap<ExerciseDto, Exercise>()
+                .ForMember(dest => dest.ExerciseId, opt => opt.Ignore())
+                .ForMember(dest => dest.Category, opt => opt.MapFrom(src => src.Category))
+                .ForMember(dest => dest.MachineId, opt => opt.MapFrom(src => src.MachineId))
+                .ForMember(dest => dest.Machines, opt => opt.Ignore())
+                .ForMember(dest => dest.RoutineExercise, opt => opt.Ignore());
         }
     }
 }
